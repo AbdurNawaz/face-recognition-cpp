802693e8738bb9a2e0a59dca80b65330c25ca03f
Abdur Nawaz <abdurnawaz.2011@gmail.com>
2024-06-12 09:02:52 +0530

    added opencv-webcam

---
+++ opencv-dlib-face-recognition.cpp
@@ -0,0 +1,27 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <unistd.h>
+
+int main(){
+
+    cv::Mat frame;
+    cv::VideoCapture cap = cv::VideoCapture(0);
+
+    if (!cap.isOpened()){
+        std::cout << "Error while opening camera";
+        return -1;
+    }
+
+    for (;;){
+        cap >> frame;
+
+        cv::imshow("Webcam", frame);
+
+        if ((char)cv::waitKey(25) == 'q'){
+            cv::destroyAllWindows();
+            break;
+        }
+    }
+    cap.release();
+}
+
---
+++ opencv-version.cpp
@@ -0,0 +1,9 @@
+#include <opencv2/core.hpp>
+#include <iostream>
+
+// using namespace cv;
+
+int main() {
+    std::cout << cv::getVersionString() << std::endl;
+    return 0;
+}
