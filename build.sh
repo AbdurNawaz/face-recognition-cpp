c8b66fa23dbc888c5d3bc289e245f70e01151490
Abdur Nawaz <abdurnawaz.2011@gmail.com>
2024-06-18 08:46:39 +0530

    added read-write

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 cmake -B build/
-arch -arm64 cmake --build build/
-./build/face-recognition-cpp
+cmake --build build/
+./build/face-recognition-cpp /Users/abdurnawaz/Documents/face-recognition-cpp/input.avi /Users/abdurnawaz/Documents/face-recognition-cpp/output.mp4
---
+++ opencv-read-write.cpp
@@ -0,0 +1,57 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+int main(int argc, char *argv[]){
+
+    if (argc < 2){
+        std::cout << "Please enter input & output location of video files" << std::endl;
+    }
+
+    cv::VideoCapture cap;
+    std::string videoInput = argv[1];
+    if (videoInput.at(0) == '0'){
+        cap = cv::VideoCapture(0);
+    } else {
+        cap = cv::VideoCapture(videoInput);
+    }
+    
+    cv::Mat frame;
+
+    if (!cap.isOpened()){
+        std::cout << "Error occured while opening stream" << std::endl;
+    }
+
+    int frame_width = static_cast<int>(cap.get(3));
+    int frame_height = static_cast<int>(cap.get(4));
+    cv::Size frame_size = cv::Size(frame_width, frame_height);
+
+    int fourcc = cv::VideoWriter::fourcc('M', 'P', '4', 'V');
+    std::string outputPath = argv[2];
+    cv::VideoWriter writer = cv::VideoWriter(outputPath, fourcc, cap.get(cv::CAP_PROP_FPS), frame_size);
+
+    std::cout << cap.isOpened() << std::endl;
+
+    while(cap.isOpened()){
+        bool isRead = cap.read(frame);
+        if (!isRead){
+            break;
+        }
+        cv::imshow("Frame", frame);
+        writer.write(frame);
+
+        int key = cv::waitKey(10);
+        if (key == 'q'){
+            break;
+        }
+    }
+
+    cap.release();
+    cv::destroyAllWindows();
+    writer.release();
+
+    std::cout << "File has been written to: " << outputPath << std::endl;
+
+    return 0;
+}
