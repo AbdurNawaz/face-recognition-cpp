5806497d59946fd614b4500a1234b0b5623b50cc
Abdur Nawaz <abdurnawaz.2011@gmail.com>
2024-06-19 08:14:31 +0530

    added libcurl example

---
+++ src/rest-client.cpp
@@ -0,0 +1,29 @@
+#include <curl/curl.h>
+#include <iostream>
+#include <string>
+
+static size_t writeCallback(char *buffer, size_t size, size_t nitems, void *outstream)
+{
+    ((std::string *)outstream)->append( buffer, size * nitems);
+    return size * nitems;
+}
+
+
+int main(){
+    CURL *handle = curl_easy_init();
+
+    std::string readBuffer;
+
+    if (handle){
+        curl_easy_setopt(handle, CURLOPT_URL, "http://localhost:8000");
+        curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, writeCallback);
+        curl_easy_setopt(handle, CURLOPT_WRITEDATA, &readBuffer);
+
+        CURLcode code = curl_easy_perform(handle);
+
+        curl_easy_cleanup(handle);
+
+        std::cout << readBuffer << std::endl;
+    }
+}
+
