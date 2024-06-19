7cf6613521d74a5d0e4e2962a649d3c9377a69c4
Abdur Nawaz <abdurnawaz.2011@gmail.com>
2024-06-19 19:34:15 +0530

    add fetch content

--- src/opencv-dlib-face-det.cpp
+++ src/opencv-dlib-face-det.cpp
@@ -1,28 +1,23 @@
 #include <dlib/image_processing/frontal_face_detector.h>
 #include <dlib/opencv.h>
 #include <opencv2/opencv.hpp>
+#include <boost/chrono.hpp>
 #include <iostream>
-
-using namespace dlib;
-using namespace std;
-using namespace cv;
 
 int main()
 {
 
-    frontal_face_detector face_detector = get_frontal_face_detector();
+    dlib::frontal_face_detector face_detector = dlib::get_frontal_face_detector();
 
-    Mat frame;
-    array2d<dlib::bgr_pixel> dlibFrame;
-    VideoCapture cap = VideoCapture(0);
+    cv::Mat frame;
+    dlib::array2d<dlib::bgr_pixel> dlibFrame;
+    cv::VideoCapture cap = cv::VideoCapture(0);
 
     if (!cap.isOpened())
     {
-        cout << "Couldn't open camera" << endl;
+        std::cout << "Couldn't open camera" << std::endl;
         return -1;
     }
-
-    namedWindow("Webcam", 1);
 
     while (true)
     {
@@ -30,21 +25,29 @@ int main()
 
         dlib::assign_image(dlibFrame, dlib::cv_image<dlib::bgr_pixel>(frame));
 
-        std::vector<dlib::rectangle> faces = face_detector(dlibFrame);
+        auto start_t = std::chrono::high_resolution_clock::now();
+
+        std::vector<dlib::rectangle> faces = face_detector(dlibFrame, 0);
+
+        auto end_t = std::chrono::high_resolution_clock::now();
+
+        double time_diff = std::chrono::duration<double, std::milli>(end_t - start_t).count();
+
+        std::cout << "Time difference: " << time_diff << std::endl;
 
         for (dlib::rectangle face : faces)
         {
-            draw_rectangle(dlibFrame, face, rgb_pixel(255, 0, 0), 4);
+            dlib::draw_rectangle(dlibFrame, face, dlib::rgb_pixel(255, 0, 0), 4);
         }
 
-        Mat changedFrame = dlib::toMat(dlibFrame);
+        cv::Mat changedFrame = dlib::toMat(dlibFrame);
 
         if (frame.empty())
         {
             break;
         }
-        imshow("image", changedFrame);
-        if ((char)waitKey(10) == 'q')
+        cv::imshow("image", changedFrame);
+        if ((char)cv::waitKey(10) == 'q')
         {
             break;
         }
